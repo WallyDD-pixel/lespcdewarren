77a33200f6c70ab3ded2c34448414171862e9a30
WallyDD-pixel <wallydibombepro@gmail.com>
2026-02-11 09:28:30 +0100

    sdcfsfsd

--- next-server-polyfill.js
+++ next-server-polyfill.js
@@ -1,11 +1,7 @@
-// Polyfill pour self, window et document dans l'environnement Node.js
-// Ce fichier est chargé avant Next.js pour définir ces variables globales
-// À charger avec : node -r ./next-server-polyfill.js ...
+// Polyfill pour self, window, document, location dans l'environnement Node.js (SSR)
+// Charger avec : node -r ./next-server-polyfill.js ...
 
-const g = typeof globalThis !== 'undefined' ? globalThis : typeof global !== 'undefined' ? global : this;
-if (typeof g.self === 'undefined') g.self = g;
-if (typeof g.window === 'undefined') g.window = g;
-// Mock window.location pour le SSR (évite "Cannot destructure property 'protocol' of 'window.location'")
+const noop = () => {};
 const fakeLocation = {
   protocol: 'https:',
   host: 'lespcdewarren.fr',
@@ -17,26 +13,35 @@ const fakeLocation = {
   hash: '',
   port: '',
 };
-if (g.window && typeof g.window.location === 'undefined') g.window.location = fakeLocation;
-// Node expose les globaux via global ; s'assurer que self/window y sont
+
+const fakeEl = () => ({
+  style: {},
+  appendChild: noop,
+  removeChild: noop,
+  setAttribute: noop,
+  addEventListener: noop,
+  removeEventListener: noop,
+  getBoundingClientRect: () => ({ top: 0, left: 0, width: 0, height: 0, bottom: 0, right: 0 }),
+});
+
+const g = typeof globalThis !== 'undefined' ? globalThis : typeof global !== 'undefined' ? global : this;
+if (typeof g.self === 'undefined') g.self = g;
+if (typeof g.window === 'undefined') g.window = g;
+g.window.location = fakeLocation;
+g.location = fakeLocation;
+g.window.addEventListener = noop;
+g.window.removeEventListener = noop;
+
 if (typeof global !== 'undefined') {
   if (typeof global.self === 'undefined') global.self = global;
   if (typeof global.window === 'undefined') global.window = global;
-  if (global.window && typeof global.window.location === 'undefined') global.window.location = fakeLocation;
+  global.window.location = fakeLocation;
+  global.location = fakeLocation;
+  global.window.addEventListener = noop;
+  global.window.removeEventListener = noop;
 }
 
-// Mock minimal de document pour le SSR (évite "document is not defined" dans certaines libs)
 if (typeof global !== 'undefined' && typeof global.document === 'undefined') {
-  const noop = () => {};
-  const fakeEl = () => ({
-    style: {},
-    appendChild: noop,
-    removeChild: noop,
-    setAttribute: noop,
-    addEventListener: noop,
-    removeEventListener: noop,
-    getBoundingClientRect: () => ({ top: 0, left: 0, width: 0, height: 0, bottom: 0, right: 0 }),
-  });
   global.document = {
     createElement: () => fakeEl(),
     createElementNS: () => fakeEl(),
